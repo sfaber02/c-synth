5953e6fe5a880dd069e3c512fd80bf07efc5d657
Shawn Faber <96348445+sfaber02@users.noreply.github.com>
2023-07-04 00:40:22 -0400

    initial commit

---
+++ src/globals.cpp
@@ -0,0 +1,4 @@
+const int SAMPLE_RATE = 44100;
+const int FREQUENCY = 400;
+const int DURATION = 5;
+const int AMPLITUDE = 400;
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "globals.cpp"
+
+int main () {
+    std::cout << "sample rate = " << SAMPLE_RATE << " that's cool" << std::endl;
+    std::cout << "amplitude = " << AMPLITUDE << " that's cool" << std::endl;
+
+    
+
+}
